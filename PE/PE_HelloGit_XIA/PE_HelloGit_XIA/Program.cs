ce2e749fdcfbbeb25d7e00f271f34e8277d8b934
BlueberryLanmm <yx5186@g.rit.edu>
2024-09-03 16:36:06 -0400

    Add_PE_HelloGit

---
+++ PE/PE_HelloGit_XIA/PE_HelloGit_XIA/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PE_HelloGit_XIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Git!");
+            Console.WriteLine("This is Yuxuan Xia.");
+        }
+    }
+}
