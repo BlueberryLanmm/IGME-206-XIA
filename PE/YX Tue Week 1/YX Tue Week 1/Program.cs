622985faaf719394611a3da79fc1c2b45b6096b8
BlueberryLanmm <yx5186@g.rit.edu>
2024-09-03 16:20:32 -0400

    First Submit

---
+++ PE/YX Tue Week 1/YX Tue Week 1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace YX_Tue_Week_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Clear();
+        }
+    }
+}
---
+++ PE/YX_Week 01_Thur/YX_Week 01_Thur/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace YX_Week_01_Thur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Console.Clear();
+
+            //Console.Write("Hi");
+            //Console.Write("there");
+            //Console.WriteLine("everybody");
+
+            //Console.WriteLine(6 + 3 * 8 - 4);
+            //Console.WriteLine(3.14159 * 8.2 * 8.2);
+
+            //Print a label and the result of an evaluated expression.
+            Console.WriteLine("Hours per week: " + 7 * 24);
+            Console.WriteLine("5" + 5); 
+        }
+    }
+}
