a0f479ddf013562f48e9d61fee100ac85d602bcb
BlueberryLanmm <yx5186@g.rit.edu>
2024-09-03 23:29:05 -0400

    Adjust File
    Clean the PE files. Fix some mistakes in previous PE assignments.

--- PE/PE_HelloGit_XIA/PE_HelloGit_XIA/Program.cs
+++ PE/PE_HelloGit_XIA/PE_HelloGit_XIA/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            //This program is a test of version control with Git.
+
             Console.WriteLine("Hello, Git!");
             Console.WriteLine("This is Yuxuan Xia.");
         }
---
+++ PE/PE_HelloWorld_XIA/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PE_HelloWorld_YX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This application will display 3 lines of words.
+            /* "Hello, GDP1."
+             * An arbitrary sentence.
+             * My name.
+             */
+            Console.WriteLine("Hello, GDP1.");
+            Console.WriteLine("I am about to take the exam out of this bridge course on Friday. Not sure if I can pass.");
+            Console.WriteLine("- Yuxuan Xia");
+        }
+    }
+}
