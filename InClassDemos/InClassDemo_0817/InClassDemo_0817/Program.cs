67a887fda03043cb77896619c33a497d4da86752
BlueberryLanmm <yx5186@g.rit.edu>
2024-09-17 16:37:11 -0400

    InClassDemo_0917

---
+++ InClassDemos/InClassDemo_0817/InClassDemo_0817/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace InClassDemo_0817
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwitchCases();
+        }
+
+        static void SwitchCases()
+        {
+            int number = 0;
+
+            switch (number)
+            {
+
+                case <1:
+                case 2:
+                    Console.WriteLine("Below 3");
+                    break;
+
+                case 0:
+
+                case 10:
+                    Console.WriteLine("Ten!");
+                    break;
+
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+
+
+        }
+    }
+}
