bd855fd8a3b0ea779a7724a6fc1b089771f4806e
BlueberryLanmm <yx5186@g.rit.edu>
2024-10-03 19:08:35 -0400

    PE_Magic 8 Ball

---
+++ PE/PE_Magic 8 Ball_XIA/PE_Magic 8 Ball_XIA/MagicEightBall.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_Magic_8_Ball_XIA
+{
+    internal class MagicEightBall
+    {
+        //Declare variables
+        private string owener;
+        private int timesShaken;
+
+        private String[] responses;
+
+        private Random rng;
+
+        /// <summary>
+        /// Constructor. Initialize the variables.
+        /// </summary>
+        /// <param name="owner">Input the owner of the ball</param>
+        public MagicEightBall(string owner)
+        {
+            owener = owner;
+            timesShaken = 0;
+
+            rng = new Random();
+
+            //Adding responses to the ball
+            responses = new String[6];
+
+            responses[0] = "It is certain.";
+            responses[1] = "As I see it, yes.";
+            responses[2] = "Cannot predict now.";
+            responses[3] = "Reply hazy, try again.";
+            responses[4] = "Don't count on it.";
+            responses[5] = "My reply is no.";
+        }
+
+        /// <summary>
+        /// Give a random respones and add up to the shaken time.
+        /// </summary>
+        /// <returns>Returns a random string in array responses</returns>
+        public string ShakeBall()
+        {
+            //Add to times shaken
+            timesShaken++;
+
+            //get a random int number ranging from 0 to 6
+            return responses[rng.Next(7)];
+        }
+
+        /// <summary>
+        /// Give out information of how many times the ball has been shaken.
+        /// </summary>
+        /// <returns>Returns the output string commenting on shaken times</returns>
+        public string Report()
+        {
+            //if shaken more than 4 times
+            if (timesShaken >= 4)
+            {
+                return String.Format(
+                    "{0} has shaken the ball {1} times. That's a lot of questions",
+                    owener, timesShaken);
+            }
+            //if shaken 1-3 times
+            else if (timesShaken >= 1)
+            {
+                return String.Format(
+                    "{0} has shaken the ball {1} times.",
+                    owener, timesShaken);
+            }
+            //if less than 1 (0 time)
+            else
+            {
+                return String.Format(
+                    "{0} has not shaken the ball yet.",
+                    owener);
+            }
+        }
+    }
+}
---
+++ PE/PE_Magic 8 Ball_XIA/PE_Magic 8 Ball_XIA/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Security.Cryptography;
+
+namespace PE_Magic_8_Ball_XIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            string owner;
+            string userInput;
+
+
+            //Print a greeting
+            Console.WriteLine("Welcome to Magic 8 Ball Simulator!");
+
+            //Prompt the user for owner name input
+            owner = GetPromptedInput(" > Who owns this ball?");
+
+            //Instantiate the MagicEightBall object
+            MagicEightBall magicEightBall = new MagicEightBall(owner);
+
+
+            //Use a loop to keep prompting user choices
+            do
+            {
+                Console.WriteLine("\nWhat would you like to do?");
+
+                //prompt user choices input
+                userInput = GetPromptedInput(
+                    "You can 'shake' the ball, get a 'report', or 'quit':")
+                    .ToLower();
+
+                switch (userInput)
+                {
+                    case "shake":
+                        //Print a random response whatever the question is
+                        //Call ShakeBall() method in MagicEightBall and print out
+                        GetPromptedInput(" > What is your question?");
+                        Console.WriteLine(" > The Magic 8 ball says: {0}", 
+                            magicEightBall.ShakeBall());
+                        break;
+
+                    case "report":
+                        //Call Report() method in MagicEightBall and print out
+                        Console.WriteLine(" > {0}", 
+                            magicEightBall.Report());
+                        break;
+
+                    case "quit":
+                        //print goodbye
+                        Console.WriteLine(" > Goodbye!");
+                        break;
+
+                    default:
+                        //print invalid information
+                        Console.WriteLine(" > I do not recongnize that response.");
+                        break;
+                }
+            }
+            //Check if the user input is "quit"
+            while (userInput != "quit");
+        }
+
+        /// <summary>
+        /// Input helper written by Prof. Mesh
+        /// Uses the given string to prompt the user for input and set
+        /// the color to cyan while they type.
+        /// </summary>
+        /// <param name="prompt">What to print before waiting for input</param>
+        /// <returns>A trimmed version of what the user entered</returns>
+        public static string GetPromptedInput(string prompt)
+        {
+            // Always print in white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Print the prompt
+            Console.Write(prompt + " ");
+
+            // Switch color and get user input (trim too)
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string response = Console.ReadLine().Trim();
+
+            // Switch back to white and then return response.
+            Console.ForegroundColor = ConsoleColor.White;
+            return response;
+        }
+    }
+}
