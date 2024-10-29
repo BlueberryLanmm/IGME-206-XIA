f1efab19668141f3ce20915cf3658341ac6f3adf
BlueberryLanmm <yx5186@g.rit.edu>
2024-10-29 16:48:49 -0400

    InClass Commit for Execise U03

---
+++ PE-U/Projects/Exercise U03/Assets/Scripts/LocateNumbers.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocateNumbers : MonoBehaviour
+{
+    private float angel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
